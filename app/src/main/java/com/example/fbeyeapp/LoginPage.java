9bec203d4285a1e53731f6fd88ba955907e1d13c
vinhee <S10221118@connect.np.edu.sg>
2024-01-22 15:52:59 +0800

    Added more pages

--- app/src/main/java/com/example/fbeyeapp/LoginPage.java
+++ app/src/main/java/com/example/fbeyeapp/LoginPage.java
@@ -1,25 +1,162 @@
 package com.example.fbeyeapp;
 
-import android.content.SharedPreferences;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+import android.text.InputType;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.widget.TextView;
+import android.app.ProgressDialog;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.TextUtils;
+import android.widget.Toast;
+
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import com.example.fbeyeapp.R;
 
 public class LoginPage extends AppCompatActivity {
+    private Button signInBtn;
+    private TextView linkSignUp;
+    FirebaseAuth mAuth;
+    private EditText emailField;
+    private EditText passwordField;
+    private TextView forgotPass;
 
-    public String MY_USERNAME="MyUserName";
-    public String MY_USERID="MyUserID";
-    SharedPreferences sharedPreferences;
-    private FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
     @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            reload();
-        }
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+
+        emailField = findViewById(R.id.loginEmailField);
+        passwordField = findViewById(R.id.passwordField);
+        forgotPass = findViewById(R.id.forgotPassword);
+
+        signInBtn = findViewById(R.id.signInBtn);
+        signInBtn.setOnClickListener(v -> {
+            String email, password;
+            email = String.valueOf(emailField.getText());
+            password = String.valueOf(passwordField.getText());
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(LoginPage.this, "Enter email", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(LoginPage.this, "Enter password", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(LoginPage.this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(LoginPage.this, "Login Success.", Toast.LENGTH_SHORT).show();
+                            Intent homeIntent = new Intent(LoginPage.this, MainActivity.class);
+                            startActivity(homeIntent);
+                            finish();
+                        }
+
+                        else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginPage.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        });
+
+        forgotPass.setOnClickListener(v -> {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Please enter your email:");
+
+
+            final EditText input = new EditText(this);
+            input.setInputType(InputType.TYPE_CLASS_TEXT);
+            input.setHint("Email Address");
+            builder.setView(input);
+
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String text = input.getText().toString();
+
+                }
+            });
+
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            AlertDialog dialog = builder.create();
+
+            // Prevent the dialog from being dismissed when the positive button is clicked
+            dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                @Override
+                public void onShow(DialogInterface dialog) {
+                    Button positiveButton = ((AlertDialog) dialog).getButton(DialogInterface.BUTTON_POSITIVE);
+                    positiveButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String inputEmail = input.getText().toString();
+                            // Process the entered text
+
+                            // If necessary conditions are met, dismiss the dialog
+                            if (inputEmail.isEmpty()) {
+                                Toast.makeText(LoginPage.this, "Please Enter Your Email", Toast.LENGTH_SHORT).show();
+                            } else {
+                                ProgressDialog progressDialog = new ProgressDialog(LoginPage.this);
+                                progressDialog.setMessage("Sending password reset email...");
+                                progressDialog.show();
+
+                                mAuth.sendPasswordResetEmail(inputEmail)
+                                        .addOnCompleteListener(task -> {
+                                            progressDialog.dismiss();
+                                            if (task.isSuccessful()) {
+                                                // Password reset email sent successfully
+                                                Toast.makeText(LoginPage.this, "Password reset email sent.", Toast.LENGTH_SHORT).show();
+                                            } else {
+                                                // Failed to send password reset email
+                                                Toast.makeText(LoginPage.this, "Failed to send password reset email.", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                dialog.dismiss();
+                            }
+                        }
+                    });
+                }
+            });
+
+            dialog.show();
+
+
+        });
+
     }
 }
--- app/src/main/java/com/example/fbeyeapp/MainActivity.java
+++ app/src/main/java/com/example/fbeyeapp/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.fbeyeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        if (item.getItemId() == R.id.home) {
+                            loadFragment(new HomeFragment());
+                            return true;
+                        } else if (item.getItemId() == R.id.stats) {
+                            loadFragment(new StatsFragment());
+                            return true;
+                        } else if (item.getItemId() == R.id.profile) {
+                            loadFragment(new ProfileFragment());
+                            return true;
+                        }
+
+                        return false;
+                    }
+
+                });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, fragment, null)
+                .commit();
     }
 }
