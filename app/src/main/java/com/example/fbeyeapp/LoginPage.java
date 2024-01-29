62ac99692c9c53f35a5f975ac8fecedd3f8a6de6
vinhee <S10221118@connect.np.edu.sg>
2024-01-29 15:52:15 +0800

    Added necessary files for firebase authentication

--- app/src/main/java/com/example/fbeyeapp/LoginPage.java
+++ app/src/main/java/com/example/fbeyeapp/LoginPage.java
@@ -47,7 +47,6 @@ public class LoginPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
 
