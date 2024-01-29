02e02b8a63e94e3b263e6d2935a0e3ff6fb1f243
vinhee <S10221118@connect.np.edu.sg>
2024-01-29 14:03:25 +0800

    Added edit profile page

--- app/src/main/java/com/example/fbeyeapp/MainActivity.java
+++ app/src/main/java/com/example/fbeyeapp/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
